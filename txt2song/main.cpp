542ef44bc2bcf134d621048d97973653670a8634
Stephen Hazel <stephen.hazel@gmail.com>
2026-09-15 07:56:35 -0700

    bumping flatpak runtime version and prepping docs for release to flathub

--- txt2song/main.cpp
+++ txt2song/main.cpp
@@ -39,14 +39,14 @@ struct {ubyt4 time;  ubyte num, den, sub;  ubyt2 bar;} TSg [128]; // timesig
 ubyt4 Time, Dur, DurArtc, TimeSave, DurMax;
 bool  Grace, Roll, Chrd;
 ubyte Octv, Velo;
-char  Scale [12], Artc,           // defaults
+TStr  Scale;                      // defaults
+char  Artc,
       DurSym  [] = "WHQEST612",   // whole, half, ... 64th, 128th, 256th
       ArtcSym [] = "<=>-",        // staccato, portato, leggerio, legato
       NoteSym [] = "C_D_EF_G_A_B";
 
 
-void Die (const char *msg)  {Die (CC(msg));}
-void Die (      char *msg)
+void Ugh (      char *msg)
 { TStr s, p;
   File f;
    StrCp (p, ErrFN);   Fn2Path (p);   Fn2Path (p);
@@ -54,6 +54,7 @@ void Die (      char *msg)
    f.Save (FNRats, s, StrLn (s));
    exit (99);
 }
+void Ugh (const char *msg)  {Ugh (CC(msg));}
 
 
 //------------------------------------------------------------------------------
@@ -99,16 +100,16 @@ char *DoNote (char *b, ubyte l)
   bool  got, nt;
   ubyte len, step, f, chd [4], nChd, i, rollOfs = 0;
    while (l && (*b == ' '))  {b++; l--;}
-   if (l == 0)                         Die ("Missin duration");
+   if (l == 0)                         Ugh ("Missin duration");
    for (len = 0;  len < l;  len++)  if (b [len] == ' ') break;
-   if (! DoDur (b, len))               Die ("Bad duration");
+   if (! DoDur (b, len))               Ugh ("Bad duration");
    b += len;  l -= len;  while (l && (*b == ' '))  {b++; l--;}
    while (l) {
       got = nt = false;
    // velocity change?
       if ((l >= 2) && (*b == 'V')) {
          step = b [1];
-         if ((step < '0') || (step > '9'))  Die ("Bad velocity");
+         if ((step < '0') || (step > '9'))  Ugh ("Bad velocity");
                   got = true;  Velo = VEL (step-'0');  b += 2;  l -= 2;
       }
    // drum?
@@ -123,8 +124,8 @@ char *DoNote (char *b, ubyte l)
    // regular octNoteNote...
       if (! got) {
          if ((*b >= '0') && (*b <= '9'))  {Octv = *b - '0';  b++;  l--;}
-         if (l == 0)                        Die ("Missin Nt after Oct");
-         if (! (p = StrCh (NoteSym, *b)))   Die ("Bad note");
+         if (l == 0)                        Ugh ("Missin Nt after Oct");
+         if (! (p = StrCh (NoteSym, *b)))   Ugh ("Bad note");
          step = p - NoteSym;                                 b++;  l--;
          if (l && StrCh (CC("%#@"), *b))  {shrp = *b;        b++;  l--;}
          else                              shrp = ' ';
@@ -135,9 +136,9 @@ char *DoNote (char *b, ubyte l)
       }
       if (nt) {
       // ok, got dur and step - create the note
-         if (NE+2 >= MAX_EVT)          Die ("Hit max events");
+         if (NE+2 >= MAX_EVT)          Ugh ("Hit max events");
          if (Grace) {
-            if (Time < (M_WHOLE/32))   Die ("Gracenote can't start bar 1");
+            if (Time < (M_WHOLE/32))   Ugh ("Gracenote can't start bar 1");
             E [NE+0].time = Time-(M_WHOLE/32);
             E [NE+1].time = Time-1;
          }
@@ -171,7 +172,7 @@ char *DoNote (char *b, ubyte l)
                                      {chd[2] = 4;  b++;  l--;}
                }
             }
-            if ((NE+2*nChd) >= MAX_EVT)  Die ("Hit max events +");
+            if ((NE+2*nChd) >= MAX_EVT)  Ugh ("Hit max events +");
             for (i = 0;  i < nChd;  i++) {
                step += chd [i];
                E [NE+0].time =                        E [NE-2].time;
@@ -195,7 +196,7 @@ char *DoCC (char *b)
 { ubyte c, i;
   ubyt2 v, n, d;
   char *p, *p2, *p3;
-   if (! (p = StrCh (b, '=')))  Die ("missing =");
+   if (! (p = StrCh (b, '=')))  Ugh ("missing =");
    *p++ = '\0';
 
 // special-ish ones...
@@ -221,11 +222,11 @@ char *DoCC (char *b)
       return nullptr;
    }
    if (! StrCm (b, "marker"))
-      Die ("use section.  not marker.  sorry.");
+      Ugh ("use section.  not marker.  sorry.");
    if (! StrCm (b, "section")) {
-      if (NMrk >= BITS (Mrk))  Die ("tooo many sections");
+      if (NMrk >= BITS (Mrk))  Ugh ("tooo many sections");
       if (NMrk && (Time <= Mrk [NMrk-1].t))
-         Die ("section's time is BEFORE previously found section - "
+         Ugh ("section's time is BEFORE previously found section - "
                "keep all sections in the same track");
       StrCp (Mrk [NMrk].s, p);   Mrk [NMrk].t = Time;
 TStr db;
@@ -235,9 +236,9 @@ NMrk, Mrk[NMrk].s, Mrk[NMrk].t, TmS (db,Mrk[NMrk].t));
       return nullptr;
    }
    if (! StrCm (b, "cue")) {
-      if (NCue >= BITS (Cue))  Die ("tooo many cue lines");
+      if (NCue >= BITS (Cue))  Ugh ("tooo many cue lines");
       if (NCue && (Time <= Cue [NCue-1].t))
-         Die ("this cue time is BEFORE a prev cue - "
+         Ugh ("this cue time is BEFORE a prev cue - "
                "keep all !cue=... stuff in the SAME track");
       StrCp (Cue [NCue].s, p);   Cue [NCue].t = Time;
       NCue++;
@@ -248,24 +249,24 @@ NMrk, Mrk[NMrk].s, Mrk[NMrk].t, TmS (db,Mrk[NMrk].t));
    for (c = 0;  c < NCtrl;  c++)  if (! StrCm (Ctrl [c], b))  break;
    if (c >= NCtrl) {                   // new dude
       if (StrLn (b) > (sizeof (WStr)-1))
-                                 Die ("control name too long");
-      if (NCtrl >= BITS (Ctrl))  Die ("too many controls");
+                                 Ugh ("control name too long");
+      if (NCtrl >= BITS (Ctrl))  Ugh ("too many controls");
       StrCp (Ctrl [NCtrl++], b);
    }
 
 // ok, parse the wierd ones first (tsig, ksig)
    if      (! StrCm (b, "TSig")) {
-      if (! (p2 = StrCh (p, '/')))     Die ("TSig Missing /");
+      if (! (p2 = StrCh (p, '/')))     Ugh ("TSig Missing /");
       p3 = StrCh (++p2, '/');
       n = (ubyt2) Str2Int (p);
       d = (ubyt2) Str2Int (p2);
       for (i = 0;  i < 8;  i++) if ((1 << i) == d) break;
-      if (i >= 8)                      Die ("bad TSig denom");
+      if (i >= 8)                      Ugh ("bad TSig denom");
       v = (i << 8) | n;
       if (p3) {                  // got subbeat?
          i = (ubyte) Str2Int (p3+1);
          if ((i >= 1) && (i <= 8))  v |= ((i-1) << 12);
-         else                          Die ("bad TSig subbeat");
+         else                          Ugh ("bad TSig subbeat");
       }
       else if (v == 0x0204)      // default 4/4 to 4/4/4
          v = 0x3204;
@@ -281,7 +282,7 @@ NMrk, Mrk[NMrk].s, Mrk[NMrk].t, TmS (db,Mrk[NMrk].t));
    else
       v = (ubyt2) Str2Int (p);         // a regular one
 
-   if (NE >= MAX_EVT)  Die ("tooo many events");
+   if (NE >= MAX_EVT)  Ugh ("tooo many events");
    E [NE].time = Time;   E [NE].ctrl = 0x80 | c;
                          E [NE].valu = (ubyte)(v & 0x00FF);
                          E [NE].val2 = (ubyte)(v >> 8);
@@ -304,7 +305,7 @@ char *DoChord (char *b, ubyte l)
       MemCp (chd, b, len);   chd [len] = '\0';
       if (StrCm (chd, "|")) {          // | really does nothin, just legibility
          if (StrCm (chd, "/")) {       // / only bumps Time
-            if (NCue >= BITS (Cue))  Die ("tooo many chords");
+            if (NCue >= BITS (Cue))  Ugh ("tooo many chords");
             Cue [NCue].t     = Time;
             Cue [NCue].s [0] = '*';   StrCp (& Cue [NCue].s [1], chd);
             NCue++;
@@ -568,7 +569,7 @@ void DoFile (char *ifn)
    if ((f.DoText (ErrFN, nullptr, DoLine))) {
       StrFmt (fn, "`s/clip/`s.txt", SPath, ifn);
       StrCp (ErrFN, fn);   ErrLine = 0;
-      if ((msg = f.DoText (ErrFN, nullptr, DoLine)))  Die (msg);
+      if ((msg = f.DoText (ErrFN, nullptr, DoLine)))  Ugh (msg);
    }
    StrCp (ErrFN, pFn);   ErrLine = pLn;
 }
@@ -588,9 +589,9 @@ char *DoLine (char *b, ubyt2 l, ubyt4 line, void *ptr)
    if (! StrCm (b, "unroll"))  {unr = 1;  return nullptr;}
    if (unr) {
       if (NSct >= BITS (Sct))
-                      Die ("too many sections in unroll");
+                      Ugh ("too many sections in unroll");
       for (i = 0;  i < NMrk;  i++)  if (! StrCm (Mrk [i].s, b))  break;
-      if (i >= NMrk)  Die ("unknown section in unroll");
+      if (i >= NMrk)  Ugh ("unknown section in unroll");
       StrCp (Sct [NSct++], b);
       return nullptr;
    }
@@ -623,10 +624,10 @@ char *DoLine (char *b, ubyt2 l, ubyt4 line, void *ptr)
 
 // default scale/artic?
    if (*b == '$')  {
-      if (l < 8)             Die ("Bad scale");
+      if (l < 8)             Ugh ("Bad scale");
       if (l > 8) {
          if (! StrCh (ArtcSym, b [8]))
-                             Die ("Bad default Artic");
+                             Ugh ("Bad default Artic");
          Artc = b [8];
       }
       Scale[0] = b[1];  Scale[2] = b[2];  Scale[4] = b[3];
@@ -667,7 +668,7 @@ TRC("arg=`s", argv [1]);
 // init globals
    NE = Time = 0;   E = & TEv [0][0];
    Octv = 4;   Velo = VEL(7);   Artc = '>';
-   MemCp (Scale, "c_d_ef_g_a_b", 12);
+   StrCp (Scale, "c_d_ef_g_a_b");
    StrCp (Path,  FN);   Fn2Path (Path);     // Path of current dir
    StrFmt (FNRats, "`s/RATS.txt", Path);   f.Kill (FNRats);
 
@@ -675,7 +676,7 @@ TRC("arg=`s", argv [1]);
    App.Path (SPath, 'd');                   // path to pianocheetah dir
 
 // read top .txt file line by line n do each track
-   if ((msg = f.DoText (FN, nullptr, DoLine)))  Die (msg);
+   if ((msg = f.DoText (FN, nullptr, DoLine)))  Ugh (msg);
 
    Put ();                             // write it alll out into a.song
 TRC("end");
